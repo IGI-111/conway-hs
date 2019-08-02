2908b2d2e2bd15478df5067d7acc290bf1c2679d
IGI-111 <igi-111@protonmail.com>
2019-08-02 23:17:37 +0200

    Use Vector

--- Main.hs
+++ Main.hs
@@ -1,72 +1,77 @@
 module Main where
-import           Data.List.Split
-import           Data.List
+import           Prelude
+import qualified Data.Vector                   as Vector
+import           Data.Vector                    ( Vector
+                                                , (!)
+                                                , fromList
+                                                , toList
+                                                )
+import           Data.List                      ( intercalate )
+import           Debug.Trace
 
-data Board = Board { field :: [Bool], width :: Int }
+data Board = Board (Vector (Vector Bool))
 type Coord = (Int, Int)
 
 instance Show Board where
-        show (Board { field = f, width = w }) = intercalate "\n" lines where
-                        lines = chunksOf w (map (\s -> if s then 'X' else '.') f)
+  show (Board v) = intercalate "\n" lines where
+    lines = map (\l -> map (\x -> if x then 'X' else '.') $ toList l) $ toList v
 
-at :: Board -> Coord -> Bool
-at b (x, y) = field b !! ((x `mod` w) + w * (y `mod` h))
- where
-  h = height b
-  w = width b
-
-neighbours :: Board -> Coord -> [Bool]
-neighbours b (x, y) = map
-  (at b)
-  [ (x - 1, y)
-  , (x + 1, y)
-  , (x    , y - 1)
-  , (x    , y + 1)
-  , (x + 1, y + 1)
-  , (x - 1, y - 1)
-  , (x + 1, y - 1)
-  , (x - 1, y + 1)
-  ]
+width :: Board -> Int
+width (Board v) = length (v ! 0)
 
 height :: Board -> Int
-height b = (length (field b)) `quot` width b
+height (Board v) = length v
 
-coords :: Board -> Int -> Coord
-coords b i = (mod i $ width b, quot i $ width b)
+at :: Board -> Coord -> Bool
+at (Board v) (x, y) = (v ! mod y (height b)) ! mod x (width b)
+  where b = Board v
+
+neighbours :: Board -> Coord -> [Bool]
+neighbours b (x, y) = v
+ where
+  v = map
+    (at b)
+    [ (x - 1, y)
+    , (x + 1, y)
+    , (x    , y - 1)
+    , (x    , y + 1)
+    , (x + 1, y + 1)
+    , (x - 1, y - 1)
+    , (x + 1, y - 1)
+    , (x - 1, y + 1)
+    ]
 
 tick :: Board -> Board
-tick b = Board
-  { field = map iterateCell (zip [1 ..] $ field b)
-  , width = width b
-  }
+tick b = Board $ Vector.map (\v -> Vector.map iterateCell v) coordinates
  where
-  iterateCell (i, alive) = alive && n >= 2 && n <= 3 || (not alive) && n == 3
-    where n = length $ filter id $ neighbours b (coords b i)
+  coordinates = fromList
+    [ fromList [ (x, y) | x <- [0 .. (width b) - 1] ]
+    | y <- [0 .. (height b) - 1]
+    ]
+  iterateCell c = (alive && (n == 2 || n == 3)) || (not alive) && n == 3
+   where
+    n     = length $ filter id $ neighbours b c
+    alive = at b c
+
 
 ticks :: Board -> Int -> [Board]
-ticks init n = foldl (\acc _ -> (tick $ head acc) : acc) [init] [1 .. n]
+ticks init n =
+  reverse $ foldl (\acc _ -> (tick $ head acc) : acc) [init] [1 .. n]
 
 main :: IO ()
 main = do
-  putStrLn $ intercalate "\n\n" $ map show $ ticks b 4
+  putStrLn $ intercalate "\n\n" $ map show $ ticks b 30
  where
-  b = Board
-    { field = [ False
-              , True
-              , False
-              , False
-              , True
-              , False
-              , False
-              , False
-              , True
-              , False
-              , True
-              , False
-              , False
-              , False
-              , False
-              , False
-              ]
-    , width = 4
-    }
+  b = Board $ fromList $ map
+    fromList
+    [ [False, False, False, False, False, False, False]
+    , [False, False, False, False, False, False, False]
+    , [False, False, False, False, False, False, False]
+    , [False, False, False, False, False, False, False]
+    , [False, False, False, False, False, False, False]
+    , [False, False, False, True, False, False, False]
+    , [False, False, True, False, False, False, False]
+    , [False, False, True, True, True, False, False]
+    , [False, False, False, False, False, False, False]
+    , [False, False, False, False, False, False, False]
+    ]
